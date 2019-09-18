4dfa609ebea04c5b8e7fd2117224662595e4f155
noeljoejohn <55492957+noeljoejohn@users.noreply.github.com>
2019-09-18 19:12:20 +0530

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly be used for he capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print('hello capstone project')
+
+
+# In[ ]:
+
+
+
+
